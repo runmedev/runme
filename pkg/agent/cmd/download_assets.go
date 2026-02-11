3b3772ba444d50412cc5bb0ef9c4b206a43c8193
Jeremy lewi <jeremy@lewi.us>
2026-02-10 17:43:21 -0800

    Add assets-dir flag for agent assets download
    Signed-off-by: Jeremy lewi <jeremy@lewi.us>

--- pkg/agent/cmd/download_assets.go
+++ pkg/agent/cmd/download_assets.go
@@ -17,6 +17,7 @@ import (
 // NewDownloadAssetsCmd downloads and unpacks the web app assets from an OCI image.
 func NewDownloadAssetsCmd(appName string) *cobra.Command {
 	var imageRef string
+	var assetsDirFlag string
 
 	cmd := &cobra.Command{
 		Use:   "download-assets",
@@ -27,32 +28,37 @@ func NewDownloadAssetsCmd(appName string) *cobra.Command {
 					return errors.New("image reference is required; set --image")
 				}
 
-				app := application.NewApp(appName)
-				if err := app.LoadConfig(cmd); err != nil {
-					return err
-				}
-				if err := app.SetupLogging(); err != nil {
-					return err
-				}
-
-				cfg := app.AppConfig.GetConfig()
-				if cfg.AssistantServer == nil {
-					return errors.New("assistantServer config must be set to download assets")
-				}
-
-				assetsDir := cfg.AssistantServer.StaticAssets
-				if assetsDir == "" {
-					homeDir, err := os.UserHomeDir()
-					if err != nil {
-						return errors.Wrap(err, "failed to resolve home directory for assets")
+				var assetsDir string
+				if assetsDirFlag != "" {
+					assetsDir = assetsDirFlag
+				} else {
+					app := application.NewApp(appName)
+					if err := app.LoadConfig(cmd); err != nil {
+						return err
 					}
-					assetsDir = filepath.Join(homeDir, "."+appName, "assets")
-				} else if !filepath.IsAbs(assetsDir) {
-					homeDir, err := os.UserHomeDir()
-					if err != nil {
-						return errors.Wrap(err, "failed to resolve home directory for assets")
+					if err := app.SetupLogging(); err != nil {
+						return err
 					}
-					assetsDir = filepath.Join(homeDir, assetsDir)
+
+					cfg := app.AppConfig.GetConfig()
+					if cfg.AssistantServer == nil {
+						return errors.New("assistantServer config must be set to download assets")
+					}
+
+					assetsDir = cfg.AssistantServer.StaticAssets
+					if assetsDir == "" {
+						homeDir, err := os.UserHomeDir()
+						if err != nil {
+							return errors.Wrap(err, "failed to resolve home directory for assets")
+						}
+						assetsDir = filepath.Join(homeDir, "."+appName, "assets")
+					} else if !filepath.IsAbs(assetsDir) {
+						homeDir, err := os.UserHomeDir()
+						if err != nil {
+							return errors.Wrap(err, "failed to resolve home directory for assets")
+						}
+						assetsDir = filepath.Join(homeDir, assetsDir)
+					}
 				}
 
 				log := zapr.NewLogger(zap.L())
@@ -73,6 +79,7 @@ func NewDownloadAssetsCmd(appName string) *cobra.Command {
 	}
 
 	cmd.Flags().StringVar(&imageRef, "image", "ghcr.io/runmedev/app-assets:latest", "OCI image reference to download (e.g. ghcr.io/runmedev/app-assets:latest)")
+	cmd.Flags().StringVar(&assetsDirFlag, "assets-dir", "", "Directory to download and unpack assets (skips config)")
 
 	return cmd
 }
